8b24cabf16c95c4ddb7554bb2a69dc0812b7b656
Atish kushwaha <atishkushwaha151@gmail.com>
2026-04-29 09:58:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
-import os
 import pandas as pd
+import os
 
 st.title("Swiggy Sales Dashboard")
 
-st.write("Files in current directory:")
+# DEBUG: show files and current path
+st.write("Current working directory:")
+st.write(os.getcwd())
+
+st.write("Files available here:")
 st.write(os.listdir())
 
+# Try loading file
 df = pd.read_excel("Swiggy Raw Data Excel.xlsx")
 
 st.dataframe(df)
