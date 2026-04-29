c65b4895b5cfa1e3fb06af9aeab8301ae6442263
Atish kushwaha <atishkushwaha151@gmail.com>
2026-04-29 10:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 import streamlit as st
 import pandas as pd
-import os
 
 st.title("Swiggy Sales Dashboard")
 
-# DEBUG: show files and current path
-st.write("Current working directory:")
-st.write(os.getcwd())
-
-st.write("Files available here:")
-st.write(os.listdir())
-
-# Try loading file
-df = pd.read_excel("Swiggy Raw Data Excel.xlsx")
+df = pd.read_excel("data.xlsx")
 
 st.dataframe(df)
+st.bar_chart(df)
