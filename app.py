9a15409c5ce18f80a465f9c83a26ee038ba829f2
Atish kushwaha <atishkushwaha151@gmail.com>
2026-05-05 15:34:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@
 import pandas as pd
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), "data.xlsx")
+file_path = os.path.join(os.path.dirname(__file__), "Swiggy Raw Data Excel.xlsx")
 
 df = pd.read_excel(file_path)
